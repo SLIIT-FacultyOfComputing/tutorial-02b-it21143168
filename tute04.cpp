9c8ee76922f40c8165d9fe05dfc891a469b8861b
it21143168 <87370336+it21143168@users.noreply.github.com>
2021-07-27 15:43:01 +0000

    Complete1

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,19 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int no)
+{
+  long fac=1;
+  int x;
+  for(x=no; x>=1; x--)
+  {
+    fac=fac*x;
+  }
+  return fac;
+}
+long nCr(int n, int r)
+{
+  long answer;
+  answer=Factorial(n)/(Factorial(r)*Factorial(n-r));
+  return answer;
+}
